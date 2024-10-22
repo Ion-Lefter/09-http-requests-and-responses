9dcd4242957bfb14e1c5a1d818b314dbcc1c881d
Ion Lefter <ionlefter1997@gmail.com>
2024-10-22 15:06:17 +0300

    222. Transforming & Using Response Data

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -1,9 +1,10 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -12,7 +13,26 @@ import { HttpClient } from '@angular/common/http';
   styleUrl: './available-places.component.css',
   imports: [PlacesComponent, PlacesContainerComponent],
 })
-export class AvailablePlacesComponent {
+export class AvailablePlacesComponent implements OnInit{
+
   places = signal<Place[] | undefined>(undefined);
   private httpClient = inject(HttpClient);
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit(): void {
+    const subscription = this.httpClient
+    .get<{places: Place[]}>('http://localhost:3000/places', {})
+    .pipe(
+      map((resData) => resData.places)
+    )
+    .subscribe({
+      next: (places) =>{
+        this.places.set(places)
+        console.log(places);
+      }
+      })
+      this.destroyRef.onDestroy(() => {
+        subscription.unsubscribe();
+    });
+  }
 }
