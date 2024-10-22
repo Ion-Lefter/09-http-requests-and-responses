f04e0aa5782a66ff6ca04bad3cecb6e3b8f6fa7e
Ion Lefter <ionlefter1997@gmail.com>
2024-10-22 17:05:10 +0300

    229. Implementing Optimistic Updating

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -44,7 +44,7 @@ export class AvailablePlacesComponent implements OnInit{
   }
 
   onSelectPlace(selectedPlace: Place){
-   const subscription = this.placesService.addPlaceToUserPlaces(selectedPlace.id).subscribe({
+   const subscription = this.placesService.addPlaceToUserPlaces(selectedPlace).subscribe({
       next: (resData: any) => console.log(resData)
     });
     this.destroyRef.onDestroy(() => {
