4185a8da27603a28e18d177de9e5cba8bfee7202
Ion Lefter <ionlefter1997@gmail.com>
2024-10-23 10:24:23 +0300

    233. Practice: Sending DELETE Requests

--- src/app/places/user-places/user-places.component.ts
+++ src/app/places/user-places/user-places.component.ts
@@ -50,5 +50,14 @@ export class UserPlacesComponent implements OnInit{
     });
 
   }
+
+  onRemovePlace(place: Place){
+    const subscription = this.placesService.removeUserPlace(place).subscribe({});
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+  });
+
+  }
 }
 
